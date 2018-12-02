9be459ec97eb80814bcdf842f79c155043a741e8
Roman Bird <romanbird@gmx.com>
2018-12-02 10:36:29 +0000

    commit day 2 part one

--- 02/one.py
+++ 02/one.py
@@ -1,2 +1,15 @@
-x = [i.strip() for i in open("input.txt").readlines()]
-print(x)
+from collections import Counter
+a=0
+b=0
+x = [Counter(i.strip()) for i in open("input.txt").readlines()]
+for i in x:
+    aDone=False
+    bDone=False
+    for v in i.values():
+        if v == 2 and not aDone:
+            aDone=True
+            a+=1
+        if v == 3 and not bDone:
+            bDone=True
+            b+=1
+print(a*b)
