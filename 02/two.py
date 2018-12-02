5b47421a522c9362faefa10c42f483e6fab667d6
Roman Bird <romanbird@gmx.com>
2018-12-02 11:53:30 +0000

    commit day two part two

--- 02/two.py
+++ 02/two.py
@@ -1,15 +1,9 @@
-from collections import Counter
-a=0
-b=0
-x = [Counter(i.strip()) for i in open("input.txt").readlines()]
-for i in x:
-    aDone=False
-    bDone=False
-    for v in i.values():
-        if v == 2 and not aDone:
-            aDone=True
-            a+=1
-        if v == 3 and not bDone:
-            bDone=True
-            b+=1
-print(a*b)
+import itertools
+x = [i.strip() for i in open("input.txt").readlines()]
+for a,b in itertools.combinations(x, 2):
+    common=str()
+    for n,i in enumerate(a):
+        if i == b[n]:
+            common+=i
+    if len(common)==len(a)-1:
+        print(common)
