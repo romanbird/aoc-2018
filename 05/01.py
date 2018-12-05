1d6458bc121f03a7edcacc0870d4808132d8c5d9
Roman Bird <romanbird@gmx.com>
2018-12-05 16:08:40 +0000

    commit day 5 part one

--- 05/01.py
+++ 05/01.py
@@ -1,13 +1,19 @@
 a=list("dabAcCaCBAcCcaDA")
-def main():
-  dirtyLoop = False
-  for n in range(len(a)-1):
-    if abs(ord(a[n])-ord(a[n+1]))==32:
-      dirtyLoop = True
-      del a[n]
-      del a[n]
-      break
-  if dirtyLoop and len(a)>1:
-    main()
-  return len(a)
-print(main())
+a = list(str([i.strip() for i in open("input.txt").readlines()][0]))
+running=True
+while running:
+    while True:
+        dirtyLoop = False
+        for n in range(len(a)-1):
+            if abs(ord(a[n])-ord(a[n+1]))==32:
+                dirtyLoop = True
+                del a[n]
+                del a[n]
+                break
+        if dirtyLoop and len(a)>1:
+            length = len(a)
+            break
+        else:
+            running=False
+            break
+print(length)
